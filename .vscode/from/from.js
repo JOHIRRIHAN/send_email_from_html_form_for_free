1eefc6e95104dfc11f5b17a63d030cd1d2ef034c
JOHIRRIHAN <jr7718301@gmail.com>
2024-01-24 10:53:49 +0600

    first commit

---
+++ .vscode/from/from.js
@@ -0,0 +1,9 @@
+function mailSend(){
+    let parms = {
+        name: document.getElementById('name').ariaValueMax,
+        email: document.getElementById('email').ariaValueMax,
+        subject: document.getElementById('subject').ariaValueMax,
+        message: document.getElementById('message').ariaValueMax,
+    }
+    
+}
